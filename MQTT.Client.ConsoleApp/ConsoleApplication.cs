9bc72cc7e99b7bfc2d74eebe9f0e8464e8742cc1
Alan Chang <alanc@alertedh.com>
2022-07-10 13:43:52 +1000

    init commit

---
+++ MQTT.Client.ConsoleApp/ConsoleApplication.cs
@@ -0,0 +1,91 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTT.Client.ConsoleApp
+{
+    class ConsoleApplication
+    {
+        public static IMqttClient MqttClient { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("MQTT Topics Watcher \n" +
+                "\n" +
+                "\t Connect to MQTT sever & topic, follow the below questions\n");
+
+            Console.WriteLine("What is host name?");
+            var host = Console.ReadLine();
+
+            Console.WriteLine("What is the topic?");
+            var topic = Console.ReadLine();
+
+            var mqttFactory = new MqttFactory();
+            MqttClient = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer(host, 1883)
+                .WithCleanSession()
+                .Build();
+
+            MqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("Connected to MQTT broker");
+                var topicFilter = new MqttTopicFilterBuilder()
+                .WithTopic("test")
+                .Build();
+
+                await MqttClient.SubscribeAsync(topicFilter);
+            });
+
+            MqttClient.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected to MQTT broker");
+            });
+
+
+            MqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine();
+                Console.WriteLine("------------Receive Message From Topic: Start -----------");
+                Console.WriteLine($"Message: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine("------------Receive Message From Topic: END ------------");
+            });
+
+            await MqttClient.ConnectAsync(options);
+
+
+            var i = 1;
+            while (i > 0)
+            {
+                Console.WriteLine("Type anything to publish the message to the topic  \n" +
+                    "To Terminate Console App, type \'CLOSE\'\n");
+
+                var messagePayload = Console.ReadLine();
+                if (messagePayload == "CLOSE")
+                    i = 0;
+                else
+                {
+                    await PublishlMessageAsync(MqttClient, messagePayload);
+                }
+            }
+        }
+
+        private static async Task PublishlMessageAsync(IMqttClient client, string messagePayload)
+        {
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("test")
+                .WithPayload(messagePayload)
+                .WithAtLeastOnceQoS()
+                .Build();
+
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+            }
+        }
+    }
+}
